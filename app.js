cd5c21d5c408767b3de3a5a3aa79aadd43c55d4e
unknown <hardeeps@damcogroup.com>
2017-06-03 18:34:05 +0530

    Remove settings.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var express = require('express')
 var session = require('express-session');
 var app = express()
 
-//app.use(flash())
-
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'twig');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -23,7 +21,6 @@ app.use(session({ cookie: { maxAge: 60000 },
                   resave: false, 
                   saveUninitialized: false}));
 
-//app.use(flash())
 				  
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
